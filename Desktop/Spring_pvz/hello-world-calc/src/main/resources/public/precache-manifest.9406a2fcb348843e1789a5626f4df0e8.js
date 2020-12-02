a0e7269dc861429cfa587b52e695ffcb3b59d021
Abcrusne <rusne.pet@gmail.com>
2020-12-02 10:02:28 +0200

    Hello world calc connected to react.

---
+++ Desktop/Spring_pvz/hello-world-calc/src/main/resources/public/precache-manifest.9406a2fcb348843e1789a5626f4df0e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff8ae361a28ae62dacbfc52aabcbb7b",
+    "url": "/hello-world-calc/index.html"
+  },
+  {
+    "revision": "c8791fec958945c43cd6",
+    "url": "/hello-world-calc/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "6dd04fb7dc36bbd7592c",
+    "url": "/hello-world-calc/static/js/2.a8174914.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/hello-world-calc/static/js/2.a8174914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f545c810e8f83c9ce55c",
+    "url": "/hello-world-calc/static/js/3.cb9a2790.chunk.js"
+  },
+  {
+    "revision": "c8791fec958945c43cd6",
+    "url": "/hello-world-calc/static/js/main.36c1fe0e.chunk.js"
+  },
+  {
+    "revision": "023d10616499fc2091b1",
+    "url": "/hello-world-calc/static/js/runtime-main.ef7822fd.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/hello-world-calc/static/media/logo.06e73328.svg"
+  }
+]);
